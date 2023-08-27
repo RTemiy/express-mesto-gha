49b5afb22f21bce905841721d7c6e3bf5e43302b
RTemiy <RTemiyGo@yandex.ru>
2023-08-27 14:04:15 +0300

    fixed user model

--- models/user.js
+++ models/user.js
@@ -44,10 +44,10 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
-  return this.findOne(email).select('+password')
+  return this.findOne(email)
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error(`Неправильные почта или пароль+${user}`));
+        return Promise.reject(new Error('Неправильные почта или пароль'));
       }
       return compare(password, user.password)
         .then((matched) => {
