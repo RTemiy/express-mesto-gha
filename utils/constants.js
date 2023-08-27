8f919fdbabcfd2f8ebd93a4856a9385aa0b24cda
RTemiy <RTemiyGo@yandex.ru>
2023-08-27 14:35:32 +0300

    fixed user model

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-const SECRET_KEY = 'smth';
+const SECRET_KEY = 'U2FsdGVkX19nV2KreWqHk1BGD+ojOGgl39N93rtj1DVVSeYcdNPGnAQt4PtU2FvJrEiPtoQRACJ0B/yIORhjvQ==';
 
 const AVATAR_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
 
